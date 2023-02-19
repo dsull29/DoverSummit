efaaf0eb709b59e1ab9c8ac9f72f79e1f535da37
dsull29 <dsull829@gmail.com>
2023-02-19 12:17:41 -0500

    initial set of code

---
+++ website/views.py
@@ -0,0 +1,12 @@
+import re
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(
+        request,
+        'website/index.html',
+        {
+            'name': 'David',
+        }
+    )
