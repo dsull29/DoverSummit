16a19bc9e285334549efcbb30a2ac6285d851461
dsull29 <dsull829@gmail.com>
2023-02-25 15:48:11 -0500

    send error message to front end

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
+import smtplib
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
-from .forms import ContactForm
+from django.utils.html import format_html
+from email.utils import formataddr
 from django.contrib import messages
 from django.conf import settings
+
+from .forms import ContactForm
 
 
 def home(request):
@@ -20,16 +24,16 @@ def handle_post(request):
 
     if form.is_valid():
         subject = "Website Inquiry"
-        body = {
-            'name': form.cleaned_data['name'],
-            'email': form.cleaned_data['email_address'],
-            'message': form.cleaned_data['message'],
-        }
-        message = "\n".join(body.values())
+        from_email = formataddr(('Dover Summit', settings.EMAIL_HOST_USER))
+        message = format_html('<p>Name: {}</p><p>Email: {}</p><p>Message: {}</p>',
+                              form.cleaned_data['name'], form.cleaned_data['email_address'], form.cleaned_data['message'])
+        recipient_list = [settings.EMAIL_HOST_USER]
 
         try:
-            send_mail(subject, message, settings.EMAIL_HOST_USER,[settings.EMAIL_HOST_USER])
+            send_mail(subject, message, from_email, recipient_list)
             messages.success(request, 'Your email has been sent successfully!')
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
+        except (BadHeaderError, smtplib.SMTPException) as e:
+            messages.error(
+                request, 'Oops! Something went wrong. Please try again later.')
+            return HttpResponse('Invalid header or SMTPException found.')
         return redirect("home")
