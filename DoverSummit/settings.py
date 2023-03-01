c2e6fc44b22fa5eaff8f94eade6e5a4b7d4087b2
dsull29 <dsull829@gmail.com>
2023-03-01 14:53:49 -0500

    add CORS handling

--- DoverSummit/settings.py
+++ DoverSummit/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,3 +141,16 @@ EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+"https://doversummit.dev",
+"https://www.doversummit.dev",
+"http://localhost:8000"
+]
+
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+# r"^https://\w+\.doversummit\.dev$",
+# ]
+
+# CORS_ALLOW_ALL_ORIGINS = False
